061becb367b45f532a778d6fa8f3b61cba0f8197
warriorminds <warrior.software.minds@gmail.com>
2017-12-20 14:26:54 -0600

    Utilizar la interfaz gráfica. (#3)

--- app/src/main/java/com/warriorminds/fundamentsactividades/ActividadPrincipal.kt
+++ app/src/main/java/com/warriorminds/fundamentsactividades/ActividadPrincipal.kt
@@ -2,11 +2,17 @@ package com.warriorminds.fundamentsactividades
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.actividad_principal.*
+import java.util.*
 
 class ActividadPrincipal : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.actividad_principal)
+
+        botonCambiarTexto.setOnClickListener {
+            tvPrincipal.setText("Número ${Random().nextInt(100)}")
+        }
     }
 }
